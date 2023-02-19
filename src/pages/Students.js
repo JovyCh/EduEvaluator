f1fa55660445bbe2d259cb8e5708360318fb5b80
JovyCh <jovyffhope@gmail.com>
2023-02-19 21:08:07 +0000

    Firebase fully functional

--- src/pages/Students.js
+++ src/pages/Students.js
@@ -1,21 +1,32 @@
-import React from 'react';
-import {table} from "../helpers/table"
-import tableItem from "../components/tableItem";
+import { useState } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import AddStudent from "../components/AddStudent";
+import StudentList from "../components/StudentList";
 
 function Students() {
+    const [studentId, setStudentId] = useState("");
+
+    const getStudentIdHandler = (id) => {
+        console.log("The ID of document to be edited: ", id);
+        setStudentId(id);
+    };
     return (
-        <div className="students">
-            <h1>Students</h1>
-            <div className="tables">
-                {table.map((tableItems, key)=>{
-                    return <tableItem id={tableItems.id}
-                                      name={tableItems.name}
-                                      gender={tableItems.Gender}
-                                      grade1={tableItems.Grade1}
-                                      grade2={tableItems.Grade2}/>
-                })}
-            </div>
-        </div>
+        <>
+        <Container style={{ width: "400px" }}>
+            <Row>
+                <Col>
+                    <AddStudent id={studentId} setStudentId={setStudentId} />
+                </Col>
+            </Row>
+        </Container>
+            <Container>
+                <Row>
+                    <Col>
+                        <StudentList getStudentId={getStudentIdHandler} />
+                    </Col>
+                </Row>
+            </Container>
+        </>
     );
 }
 
